1ac9bc8baa1a0c8dfbd05e44f019c54bbc20f389
Kirill Klimov <ra22341@ya.ru>
2019-08-20 10:06:32 +0300

    example added

---
+++ examples/grid_example.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import np_draw_tools
+
+
+def main():
+    img1 = np.zeros((256, 128, 3), dtype=np.uint8)
+    img2 = np.zeros((256, 128, 3), dtype=np.uint8)
+    img3 = np.zeros((256, 128, 3), dtype=np.uint8)
+
+    cv2.circle(img1, (64, 40), radius=10, color=(0, 255, 0), thickness=-1)
+
+    img2[:] = 255
+    cv2.circle(img2, (5, 60), radius=10, color=(255, 255, 0), thickness=-1)
+
+    img3[:] = 128
+    cv2.circle(img3, (128, 69), radius=10, color=(0, 0, 255), thickness=-1)
+
+    grid1 = np_draw_tools.make_grid([img1, img2, img3, img3, img1, img2])
+    grid2 = np_draw_tools.make_grid([img1, img2, img3, img3, img1, img2], n_items_in_row=3)
+    grid3 = np_draw_tools.make_grid([img1, img2, img3, img3, img1, img2], n_items_in_row=2)
+
+    cv2.imshow("grid1", grid1)
+    cv2.imshow("grid2", grid2)
+    cv2.imshow("grid3", grid3)
+    cv2.waitKey()
+
+
+if __name__ == '__main__':
+    main()
